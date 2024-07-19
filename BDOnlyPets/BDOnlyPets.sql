30920408df2f806e0b8900e2f17fdd80be5feed5
Bruno Submarinho <brunomarinhosouza16@gmail.com>
2024-07-19 05:11:14 -0300

    adicionando o banco de dados

---
+++ BDOnlyPets/BDOnlyPets.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE sistema_cadastro;
+USE sistema_cadastro;
+
+CREATE TABLE usuarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    senha VARCHAR(255) NOT NULL,
+    telefone VARCHAR(15) NOT NULL
+);
+
+ALTER TABLE usuarios ADD COLUMN endereco VARCHAR(255);
+
+
+CREATE TABLE animais (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    especie VARCHAR(255) NOT NULL,
+    idade INT NOT NULL,
+    descricao TEXT NOT NULL,
+    genero ENUM('Masculino', 'Feminino') NOT NULL
+);
+
+show tables;

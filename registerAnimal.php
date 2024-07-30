3cb71cdf0ec2665012d4298fc9ec6d8c5e0f4f17
reiynto <yanf364o@gmail.com>
2024-07-30 09:59:21 -0400

    inclusao da coluna de preço no html,php e banco de dados

--- registerAnimal.php
+++ registerAnimal.php
@@ -8,15 +8,15 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $descricao = $_POST['descricao'];
     $genero = $_POST['genero'];
     $opcao = $_POST['opcao'];
-
+    $preco = $_POST ['preco'];
     // Tratamento do upload da imagem
     $foto = $_FILES['foto']['name'];
     $target_dir = "uploads/";
     $target_file = $target_dir . basename($foto);
     if (move_uploaded_file($_FILES['foto']['tmp_name'], $target_file)) {
         // Insere os dados no banco de dados
-        $sql = "INSERT INTO animais (nome, especie, idade, descricao, genero, foto, opcao_compra) 
-                VALUES ('$nome', '$especie', $idade, '$descricao', '$genero', '$foto', '$opcao')";
+        $sql = "INSERT INTO animais (nome, especie, idade, descricao, genero, foto, opcao_compra,preco) 
+                VALUES ('$nome', '$especie', $idade, '$descricao', '$genero', '$foto', '$opcao','$preco')";
 
         if ($conn->query($sql) === TRUE) {
             echo "Novo registro criado com sucesso";
